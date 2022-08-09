b1403b396a715b2eeef326d84205d12dbcfc63f8
chen yang <awschen@amazon.com>
2022-08-09 16:31:33 -0700

    lambda

--- cdk_codebuild_stepfunction/cdk_codebuild_stepfunction_stack.py
+++ cdk_codebuild_stepfunction/cdk_codebuild_stepfunction_stack.py
@@ -1,7 +1,12 @@
+import aws_cdk as cdk
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda,
+    aws_iam,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
+    aws_codebuild as codebuild
 )
 from constructs import Construct
 
@@ -10,10 +15,21 @@ class CdkCodebuildStepfunctionStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+# Grant Lambda permissions for StartBuild and BatchGet* actions for codebuild resources
+        codebuild_statement=aws_iam.PolicyStatement(
+            actions=["codebuild:StartBuild","codebuild:BatchGet*"],
+            resources=["*"]
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkCodebuildStepfunctionQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+# Helper lambda to call CodeBuild projects in Step Functions
+        codebuild_lambda = aws_lambda.Function(
+            self, "codebuild-function",
+            function_name="codebuild-function",
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            handler="codebuild_lambda.handler",
+            code=aws_lambda.Code.from_asset("./lambdas"),
+            timeout=cdk.Duration.minutes(10)
+        )
+
+# Attach CodeBuild policy to Lambda execution role
+        codebuild_lambda.role.add_to_policy(codebuild_statement)
